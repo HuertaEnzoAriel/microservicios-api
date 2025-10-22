e0991354dc4f18e153ba54076b5f4f3bd047a424
HuertaEnzoAriel <ehuerta@iinfo.unsj.edu.ar>
2025-10-22 20:11:41 -0300

    agregar metodos label y values para usabilidad

--- app/Enums/PostStatus.php
+++ app/Enums/PostStatus.php
@@ -8,4 +8,22 @@ enum PostStatus: string
     case APPROVED_BY_MODERATOR = 'approved_by_moderator';
     case SCHEDULED = 'scheduled';
     case ARCHIVED = 'archived';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+
+    /**
+     * Etiquetas legibles para humanos
+     */
+    public function label(): string
+    {
+        return match($this) {
+            self::DRAFT => 'Borrador',
+            self::APPROVED_BY_MODERATOR => 'Aprobado',
+            self::SCHEDULED => 'Programado',
+            self::ARCHIVED => 'Archivado',
+        };
+    }
 }
