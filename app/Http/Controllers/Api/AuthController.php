e39e83d44d4e525da347b065d3525d237e5c0167
HuertaEnzoAriel <ehuerta@iinfo.unsj.edu.ar>
2025-11-03 02:10:48 -0300

    feat: Add first_name and last_name fields to user registration and model; update tests and seeders

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -20,6 +20,8 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
+            'first_name' => 'required|string|max:100',
+            'last_name' => 'required|string|max:100',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
         ]);
@@ -34,6 +36,8 @@ class AuthController extends Controller
 
         $user = User::create([
             'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
